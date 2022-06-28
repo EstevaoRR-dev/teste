f23bb58162974b8f6ae6e2dff40bc85479551bbc
Estêvão Roldão <estevaor_roldao@hotmail.com>
2022-06-28 13:11:58 -0300

    Aficiona config Netlify

---
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,35 @@
+import { UsuarioExisteService } from './usuario-existe.service';
+import { NovoUsuarioService } from './novo-usuario.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { minisculoValidator } from './minusculo.validator';
+
+@Component({
+  selector: 'app-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css']
+})
+export class NovoUsuarioComponent implements OnInit {
+
+  novoUsuarioForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService,
+    private usuarioExistenteService: UsuarioExisteService) { }
+
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+       fullName: ['', [Validators.required, Validators.minLength(4)]],
+        userName:['', [minisculoValidator],
+        this.usuarioExistenteService.usuarioJaExite()],
+         password: [''],
+    });
+  }
+
+  cadastrar(){
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+  }
+
+}
